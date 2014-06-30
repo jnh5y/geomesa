f3f7bcc9d98ceae446697dd2d96fe9cea55b492e
Jim Hughes <jnh5y@ccri.com>
2014-06-30 09:44:56 -0400

    Friday's work on BBOX Gen.

--- geomesa-core/src/test/scala/geomesa/core/filter/FilterGenerator.scala
+++ geomesa-core/src/test/scala/geomesa/core/filter/FilterGenerator.scala
@@ -1,8 +1,12 @@
 package geomesa.core.filter
 
+import geomesa.utils.geohash.BoundingBox
 import org.geotools.factory.CommonFactoryFinder
 import org.geotools.filter.text.ecql.ECQL
 import org.opengis.filter.Filter
+import org.scalacheck.Gen
+import scala.collection.immutable.NumericRange
+import scala.runtime.RangedProxy
 
 object FilterGenerator {
 
@@ -21,6 +25,34 @@ object FilterGenerator {
   def intToAttributeFilter(i: Int): Filter = s"attr$i = val$i"
   implicit def intToFilter(i: Int): RichFilter = intToAttributeFilter(i)
 
+  val maxLon = 50
+  val minLon = 40
+  val maxLat = 30
+  val minLat = 20
+
+  def br[T](min: RangedProxy[T], max: T)(implicit ev: RangedProxy[T]#ResultWithoutStep <:< NumericRange[T]) = Gen.pick(2, min to max)
+
+  //type NR = A forSome { type T :: type RangedProxy[T]#ResultWithoutStep <: Seq }
+
+
+  type A = B forSome { type B <: RangedProxy[B] }
+
   // Generate filters via ScalaCheck.
+
+  //def buildRange[T forSome { type T}](min: RangedProxy[T], max: T)(implicit num: RangedProxy[T]#ResultWithoutStep <:< NumericRange[T]) = Gen.pick(2, min to max)
+
+  def buildRange(min: Int, max: Int): Gen[(Int, Int)] = Gen.pick(2, min to max).map(a=> (a(0), a(1)))
+
+  def genBBOX(): Gen[BoundingBox] = {
+    val lat = buildRange(minLat, maxLat)
+    val lon = buildRange(minLon, maxLon)
+
+    val g = for {
+      (minX, maxX) <- lon
+      (minY, maxY) <- lat
+    } yield BoundingBox(minX, maxX, minY, maxY)
+
+    g
+  }
 
 }
