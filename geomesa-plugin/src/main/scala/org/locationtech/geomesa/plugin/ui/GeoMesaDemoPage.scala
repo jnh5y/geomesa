45ae5902aafdba65482d09588cafa24c0cc0f85c
Jim Hughes <jnh5y@ccri.com>
2014-10-31 19:02:49 -0400

    Sample code to add a link to the side.
    Notes:
    
    1.  The link had to be wired up via Spring (applicationContext.xml).
    2.  The title and other words were configured in the .properties file.
    3.  Note that the html and Java class need to have the same package.  This is required, not an accident or convenience.

---
+++ geomesa-plugin/src/main/scala/org/locationtech/geomesa/plugin/ui/GeoMesaDemoPage.scala
@@ -0,0 +1,7 @@
+package org.locationtech.geomesa.plugin.ui
+
+import org.apache.wicket.markup.html.basic.Label
+
+class GeoMesaDemoPage extends GeoMesaBasePage {
+  add(new Label("label", "Hello World"))
+}
